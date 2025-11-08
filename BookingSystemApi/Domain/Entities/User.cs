2c920c8ed921c5cad462bd83bf5d60baccefdeec
Oleksii Bordiuzhevych <balex0121@gmail.com>
2025-11-08 13:33:51 +0200

    Install packages MediatR in Application and Microsoft.EntityFrameworkCore in Infrastructure. Create UserRole enum file in Domain and add role enums there. Delete unnecessary build named Presentation

--- BookingSystemApi/Domain/Entities/User.cs
+++ BookingSystemApi/Domain/Entities/User.cs
@@ -6,5 +6,4 @@ public class User
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public string Role { get; set; } = string.Empty;
 }
