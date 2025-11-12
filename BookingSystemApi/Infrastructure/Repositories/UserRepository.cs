6394287dfff1625dd0d86c03eb75f4be0aeca64d
Oleksii Bordiuzhevych <balex0121@gmail.com>
2025-11-12 21:51:54 +0200

    Introduced AppDbContext class for database access and UserRepository class that implements IUserRepository with methods for searching and adding users asynchronously

---
+++ BookingSystemApi/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository(AppDbContext db) : IUserRepository
+{
+    private readonly AppDbContext _db = db;
+
+    public Task<User?> GetByEmailAsync(string email) => _db.Users.FirstOrDefaultAsync(u => u.Email == email);
+
+    public async Task AddUserAsync(User user)
+    {
+        _db.Users.Add(user);
+        await _db.SaveChangesAsync();
+    }
+}
