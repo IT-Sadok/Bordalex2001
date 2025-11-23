e3163a0c5d7697503954dba90565a2e5902db2f0
Oleksii Bordiuzhevych <balex0121@gmail.com>
2025-11-23 16:16:16 +0200

    Introduce mediator-like operation handling: add IRequest<TResult>, IOperationHandler<TRequest, TResult> and OperationExecutor for centralized handler resolution and execution.

---
+++ BookingSystemApi/Application/Common/Mediator/IOperationHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Common.Mediator;
+
+public interface IOperationHandler<TRequest, TResult>
+    where TRequest : IRequest<TResult>
+{
+    Task<TResult> HandleAsync(TRequest request, CancellationToken ct = default);
+}
---
+++ BookingSystemApi/Application/Common/Mediator/OperationExecutor.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Application.Common.Mediator;
+
+public class OperationExecutor(IServiceProvider serviceProvider)
+{
+    public async Task<TResult> ExecuteAsync<TRequest, TResult>(TRequest request, CancellationToken ct = default)
+        where TRequest : IRequest<TResult>
+    {
+        if (request is null)
+        {
+            throw new ArgumentNullException(nameof(request));
+        }
+        var handlerObj = serviceProvider.GetService(typeof(IOperationHandler<TRequest, TResult>));
+        if (handlerObj is not IOperationHandler<TRequest, TResult> handler)
+        {
+            throw new InvalidOperationException($"No handler found for request of type {typeof(TRequest).FullName}");
+        }
+        return await handler.HandleAsync(request, ct);
+    }
+}
