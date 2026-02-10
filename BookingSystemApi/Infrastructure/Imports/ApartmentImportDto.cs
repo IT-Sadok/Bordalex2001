c459f2d3bf5c405b7b940cf39ae9cab0f4e9b8f4
Oleksii Bordiuzhevych <balex0121@gmail.com>
2026-02-10 14:12:08 +0200

    Add ApartmentImportDto and HostImportDto classes to import apartment and host data respectively

---
+++ BookingSystemApi/Infrastructure/Imports/ApartmentImportDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Imports;
+
+public class ApartmentImportDto
+{
+    public string ExternalId { get; set; } = null!;
+    public string HostExternalId { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public decimal PricePerNight { get; set; }
+    public bool IsAvailable { get; set; }
+}
