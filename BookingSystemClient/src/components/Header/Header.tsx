c5fb8ed8ba474283e4fca3a9005e7652c9000c1a
Oleksii Bordiuzhevych <balex0121@gmail.com>
2026-08-03 23:59:37 +0300

    Refactor Header component for improved navigation structure and styling

--- BookingSystemClient/src/components/Header/Header.tsx
+++ BookingSystemClient/src/components/Header/Header.tsx
@@ -1,16 +1,36 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <>
-            <header>
-                <h1>Booking System</h1>
-                <nav>
-                    <Link to="/">Home</Link> |
-                    <Link to="/login">Login</Link> |
-                    <Link to="/register">Register</Link>
-                </nav>
-            </header>
-        </>
-    )
+
+  return (
+    <>
+      <header>
+        <nav
+          aria-label="Main Navigation"
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          role="navigation"
+        >
+          <div className="flex lg:flex-1">
+            <Link to="/" className="-m-1.5 p-1.5">
+              Booking System
+            </Link>
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-x-12">
+            <Link
+              to="/login"
+              className="text-sm/6 font-semibold text-gray-900 hover:text-blue-600"
+            >
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="text-sm/6 font-semibold text-gray-900 hover:text-blue-600"
+            >
+              Register
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
