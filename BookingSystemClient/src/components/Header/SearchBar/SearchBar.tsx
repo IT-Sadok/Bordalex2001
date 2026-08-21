2ade30558f6d50ea9412eb24bceaa83bd5ff8ce6
Oleksii Bordiuzhevych <balex0121@gmail.com>
2026-08-22 02:11:41 +0300

    Create DatePicker component for date selection in SearchBar; integrate with useClickOutside hook

--- BookingSystemClient/src/components/Header/SearchBar/SearchBar.tsx
+++ BookingSystemClient/src/components/Header/SearchBar/SearchBar.tsx
@@ -1,3 +1,4 @@
+import DatePicker from "./DatePicker";
 import DestinationPicker from "./DestinationPicker";
 
 export default function SearchBar() {
@@ -9,12 +10,7 @@ export default function SearchBar() {
           <div className="flex min-w-0 flex-1 items-center">
             <DestinationPicker />
             <span className="border-l border-gray-300 h-6" />
-            <button
-              type="button"
-              className="min-w-0 flex-1 truncate px-2 py-2 text-center text-sm font-medium sm:px-3 sm:text-base hover:bg-gray-100 rounded-2xl"
-            >
-              Any week
-            </button>
+            <DatePicker />
             <span className="border-l border-gray-300 h-6" />
             <button
               type="button"
