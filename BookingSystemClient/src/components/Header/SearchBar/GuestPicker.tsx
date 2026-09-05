f2639e903c8cbc2d3a3e059bb807c758f825a2e0
Oleksii Bordiuzhevych <balex0121@gmail.com>
2026-09-05 17:29:47 +0300

    Add guest selection functionality to GuestPicker component

--- BookingSystemClient/src/components/Header/SearchBar/GuestPicker.tsx
+++ BookingSystemClient/src/components/Header/SearchBar/GuestPicker.tsx
@@ -4,6 +4,11 @@ import useClickOutside from "../../../hooks/useClickOutside";
 export default function GuestPicker() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const [adults, setAdults] = useState(0);
+  const [children, setChildren] = useState(0);
+  const [infants, setInfants] = useState(0);
+  const [pets, setPets] = useState(0);
+
   const guestPickerRef = useRef<HTMLDivElement>(null);
 
   useClickOutside(guestPickerRef, () => {
@@ -25,6 +30,41 @@ export default function GuestPicker() {
         {isOpen && (
           <div className="absolute right-0 top-full z-10 mt-2 p-6 w-80 rounded-2xl bg-white shadow-lg">
             <p className="mb-3 text-sm font-semibold">Who is coming?</p>
+            <div className="flex items-center justify-between">
+              <div>
+                <div className="font-medium">Adults</div>
+                <div className="text-sm text-gray-500">Ages 18 or above</div>
+              </div>
+              <div className="flex items-center gap-3">
+                <button
+                  type="button"
+                  className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300 transition-all"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="size-4"
+                  >
+                    <path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5h-8.5Z" />
+                  </svg>
+                </button>
+                <span>{adults}</span>
+                <button
+                  type="button"
+                  className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300 transition-all"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="size-4"
+                  >
+                    <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
           </div>
         )}
       </div>
